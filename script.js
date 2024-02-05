ea8488cb5ebf74ba9d481bca7e1915bde26f006d
marcelo <marcelomartins.escorpiao@gmail.com>
2024-02-05 14:42:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function outra() {
+  window.location.href = "index.html"
+}
+
+function openPopup() {
+  document.getElementById("popup").style.display = "block"
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none"
+}
